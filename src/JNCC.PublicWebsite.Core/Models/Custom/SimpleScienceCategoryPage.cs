23d99c519745ebf03d8a85159ed73f45a0c675e2
Alex Wilks <awilks@weareiterate.com>
2026-09-10 16:59:18 +0100

    Fix issues with the simple pages
    * Prevent SimpleScienceDetailsPage (and SimpleScienceCategoryPage) from appearing in nav
    * Ensure SimpleScienceDetailsPage (and SimpleScienceCategoryPage) appear on A-Z page

---
+++ src/JNCC.PublicWebsite.Core/Models/Custom/SimpleScienceCategoryPage.cs
@@ -0,0 +1,5 @@
+﻿namespace JNCC.PublicWebsite.Core.Models
+{
+    public partial class SimpleScienceCategoryPage : IScienceCategorisablePage
+    { }
+}
---
+++ src/JNCC.PublicWebsite.Core/Models/Custom/SimpleScienceDetailsPage.cs
@@ -0,0 +1,12 @@
+﻿using DocumentFormat.OpenXml.EMMA;
+using Umbraco.Cms.Core.Models;
+
+namespace JNCC.PublicWebsite.Core.Models
+{
+    public partial class SimpleScienceDetailsPage : IScienceCategorisablePage
+    {
+        public string CategoryOrderingName => string.Empty;
+
+        public MediaWithCrops HeroImage => null;
+    }
+}
